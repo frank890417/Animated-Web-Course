529010ba543f9c65e34c252fb2e3b9e17765da4e
majer666 <frank890417@gmail.com>
2016-10-14 19:58:13 +0800

    add cache feature for fetching web

--- api/parse_codepen.php
+++ api/parse_codepen.php
@@ -7,6 +7,7 @@
   header('Access-Control-Allow-Origin: *');  
   	// mb_internal_encoding('UTF-8');
 
+  //determine args
   $type="json";
 
   if (isset($_GET["type"]))
@@ -15,9 +16,38 @@
   $urls=$_GET["urls"];
   $alldata=array();
 
+  $cachetxt=file_get_contents("data/codepen_cache.txt");
+  $cache=json_decode($cachetxt);
+
+  // print_r($cache);
+
+  //parsing all url in urls
   for($i=0;$i<count($urls);$i++){
-    array_push($alldata,parsePen($urls[$i]));
+    $now_url=$urls[$i] ;
+    $re = '/pen\/([a-zA-Z]*)/';
+    $str = $now_url;
+
+    preg_match_all($re, $str, $matches);
+    $key_url=$matches[1][0];
+    // Print the entire match result
+    // print_r($matches);
+
+    // print_r($cache->$key_url);
+
+    if (is_null ($cache->$key_url)){
+       // echo "parse!!! ".$key_url."<br>";
+       $parse_data=parsePen($now_url);
+       // echo $parse_data;
+       $cache->$key_url=$parse_data;
+    }else{
+      // print_r($cache->$key_url);
+      // echo $cache->$key_url;
+    }
+
+    array_push($alldata,$cache->$key_url);
+
   }
+  file_put_contents("data/codepen_cache.txt", json_encode($cache));
 
   // Print the entire match result
   if ($type=="json"){
@@ -27,15 +57,19 @@
       print_r($alldata);
   }
 
-  
+  //get pen data
   function parsePen($url){
     //parse meta og data
-    $alltext =  file_get_contents($url);
+    $parse_url=str_replace("/pen/", "/full/",$url );
+
+    $alltext =  file_get_contents($parse_url);
     $re = '/(og.*)\" content=\"(.*?)\"/';
     preg_match_all($re, $alltext, $matches);
 
 
     $data=array();
+    $data["url"]=$url;
+    // print_r($matches);
     for($i=0;$i<count($matches[1]);$i++){
       $dataname=str_replace(":","_",$matches[1][$i]);
       $data[$dataname]=$matches[2][$i];
